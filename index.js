21a70ea9bdeb34bfd293760929638a8c1bb8fe4a
toxic-nubbie <69045566+toxic-nubbie@users.noreply.github.com>
2020-08-02 17:52:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const db = require('better-sqlite3')('foobar.db', options);
 const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
 console.log(row.firstName, row.lastName, row.email);
 
-const Enmap = require("enmap");
-client.points = new Enmap({name: "points"});
+const Enmap = require('enmap');
+client.points = new Enmap({name: 'points'});
 
 
 client.once('ready', () => {
