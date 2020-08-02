842c5226a87ceab6ade0b99202378a8cf3b04c12
toxic-nubbie <69045566+toxic-nubbie@users.noreply.github.com>
2020-08-02 17:23:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "ns."
+const Enmap = require("enmap");
+client.points = new Enmap({name: "points"});
+
 
 client.once('ready', () => {
     console.log(`logged in as ${client.user.username}`)
@@ -68,6 +71,46 @@ client.on('message', message => {
     }
 
     
+});
+
+client.on("message", message => {
+  // As usual, ignore all bots.
+  if (message.author.bot) return;
+  
+  // If this is not in a DM, execute the points code.
+  if (message.guild) {
+    // We'll use the key often enough that simplifying it is worth the trouble.
+    const key = `${message.guild.id}-${message.author.id}`;
+
+    // Triggers on new users we haven't seen before.
+    client.points.ensure(`${message.guild.id}-${message.author.id}`, {
+      user: message.author.id,
+      guild: message.guild.id,
+      points: 0,
+      level: 1
+    });
+    
+    client.points.inc(key, "points");
+    
+    // Calculate the user's current level
+    const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, "points")));
+    
+    // Act upon level up by sending a message and updating the user's level in enmap.
+    if (client.points.get(key, "level") < curLevel) {
+      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+      client.points.set(key, curLevel, "level");
+    }
+  }
+  
+      if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+   if (command === "points") {
+    const key = `${message.guild.id}-${message.author.id}`;
+    return message.channel.send(`You currently have ${client.points.get(key, "points")} points, and are level ${client.points.get(key, "level")}!`);
+  }    
 });
 
 client.login(process.env.token)
