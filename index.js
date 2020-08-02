10aa1e4d0fde6dff9fa323999af122f782b544d9
toxic-nubbie <69045566+toxic-nubbie@users.noreply.github.com>
2020-08-02 17:48:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "ns."
+
+const db = require('better-sqlite3')('foobar.db', options);
+ 
+const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
+console.log(row.firstName, row.lastName, row.email);
+
 const Enmap = require("enmap");
 client.points = new Enmap({name: "points"});
 
